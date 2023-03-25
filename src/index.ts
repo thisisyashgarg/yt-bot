0bb758fd2cc8c9a226e1c2a7b327559cbe477270
Yash Garg <thisisyashgarg@gmail.com>
2023-03-26 00:17:30 +0530

    removed commented code

--- src/index.ts
+++ src/index.ts
@@ -4,60 +4,6 @@ import fetch from "node-fetch";
 
 console.log("code has started running");
 
-type SpotifyTracksData = {
-  albums: {
-    href: string;
-    items: [];
-  };
-};
-// import { path as ffmpegPath } from "@ffmpeg-installer/ffmpeg";
-// import ffmpeg from "fluent-ffmpeg";
-// ffmpeg.setFfmpegPath(ffmpegPath);
-// import videoshow from "videoshow";
-
-// var images = ["ima.jpg"];
-
-// var videoOptions = {
-//   fps: 25,
-//   loop: 5, // seconds
-//   transition: true,
-//   transitionDuration: 1, // seconds
-//   videoBitrate: 1024,
-//   videoCodec: "libx264",
-//   size: "640x?",
-//   audioBitrate: "128k",
-//   audioChannels: 2,
-//   format: "mp4",
-//   pixelFormat: "yuv420p",
-// };
-
-// videoshow(images, videoOptions)
-//   .audio("audio.wav")
-//   .save("video.mp4")
-//   .on("start", function (command) {
-//     console.log("ffmpeg process started:", command);
-//   })
-//   .on("error", function (err, stdout, stderr) {
-//     console.error("Error:", err);
-//     console.error("ffmpeg stderr:", stderr);
-//   })
-//   .on("end", function (output) {
-//     console.error("Video created in:", output);
-//   });
-
-// Function to fetch the lyrics for a song
-// const getLyrics = async () => {
-//   try {
-//     console.log("get lyrics  called");
-//     const data = await fetch("https://lyrist.vercel.app/api/yellow/coldplay");
-//     // const response = await fetch(lyricsEndpoint);
-//     console.log(await data.json());
-//     // return response;
-//   } catch (err) {
-//     console.log(`Error fetching lyrics for song ${err}`);
-//   }
-// };
-
 //get top songs
 const getTopSongs = async () => {
   try {
@@ -80,40 +26,3 @@ const getTopSongs = async () => {
 };
 
 getTopSongs();
-
-// getLyrics();
-
-// Function to create a lyrical video for a song
-// const createLyricalVideo = async (songId) => {
-//   try {
-//     console.log("create lyrical video called");
-
-//     const lyrics = await getLyrics(songId);
-//     const mp3 = await getMP3(songId);
-
-//     // Write the MP3 file to disk
-//     writeFileSync(`${songId}.mp3`, mp3);
-
-//     // Create the lyrical video
-//     ffmpeg()
-//       .input(`${songId}.mp3`)
-//       .input(`lyrics.txt`)
-//       .output(`${songId}.mp4`)
-//       .outputOption(`-c:v libx264`)
-//       .outputOption(`-c:a aac`)
-//       .outputOption(`-strict experimental`)
-//       .outputOption(`-b:a 192k`)
-//       .outputOption(`-pix_fmt yuv420p`)
-//       .outputOption(`-shortest`)
-//       .outputOption(`-filter_complex "[0:a][1:v] overlay=0:0"`)
-//       .on("end", () => {
-//         console.log(`Lyrical video for ${songId} created successfully!`);
-//       })
-//       .on("error", (err) => {
-//         console.log(`Error creating lyrical video for ${songId}: ${err}`);
-//       })
-//       .run();
-//   } catch (err) {
-//     console.log(`Error creating lyrical video for ${songId}: ${err}`);
-//   }
-// };
